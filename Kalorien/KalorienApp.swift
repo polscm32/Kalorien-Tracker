54701bc4f7b1eb43e831b3fe453423a5f8dc0a30
polscm32 <107251660+polscm32@users.noreply.github.com>
2023-11-11 17:28:42 +0100

    Initial Commit

---
+++ Kalorien/KalorienApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalorienApp.swift
+//  Kalorien
+//
+//  Created by Marvin Polscheit on 11.11.23.
+//
+
+import SwiftUI
+
+@main
+struct KalorienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
